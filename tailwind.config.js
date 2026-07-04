9fc747ca7e67c83ae7f457283b4220282d8f1de7
mikobiscante <michael.biscante@gmail.com>
2026-07-04 23:03:18 +0800

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./style/**/*.{css,js,ts,jsx,tsx,mdx}",
+    "./styles/**/*.{css,js,ts,jsx,tsx,mdx}",
+    "./**/*.{html,js,jsx,ts,tsx,mdx,css}"
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {}
+  },
   plugins: []
 };
